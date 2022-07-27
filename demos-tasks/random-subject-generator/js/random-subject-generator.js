564accb3845832a7c8a44b0a2680df97196a06f9
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2022-07-27 08:52:18 +0200

    Create random-subject-generator.js

---
+++ demos-tasks/random-subject-generator/js/random-subject-generator.js
@@ -0,0 +1,28 @@
+var uniqueID; 
+
+    var randomGen = {
+      
+      type: jsPsychCallFunction,
+      func: function(){
+        var IDLetter = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Y', 'Z']
+        var IDLetterChoose = jsPsych.randomization.shuffle(IDLetter).slice(0, 3).join('')
+        var randomNumberID = `${Math.floor((Math.random()*1000))}`
+        uniqueID = `${Math.floor((Math.random()*100))}`+`-${IDLetterChoose}`+`-${Math.floor(Date.now()-1000000000000)}`; 
+        console.log(uniqueID)
+        jsPsych.data.addProperties({subject: uniqueID});
+        console.log(Date(Date.now()).toLocaleString().split(',')[0])
+        return uniqueID
+      },
+    };
+    
+    timeline.push(randomGen);
+    
+    var displayGen = {
+      type: jsPsychHtmlButtonResponse,
+      stimulus: function(){
+        return `<p>Exemple de numéro:</p><p style="font-size: 30px">${uniqueID}</p><hr>`
+      },
+      choices: ['Continuer']
+    };
+
+    timeline.push(displayGen);
