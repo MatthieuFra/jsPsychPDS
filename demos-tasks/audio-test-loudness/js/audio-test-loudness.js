ed62e7d548e245de295b8dc0b082d69356202b11
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2022-07-27 08:55:55 +0200

    Create audio-test-loudness.js

---
+++ demos-tasks/audio-test-loudness/js/audio-test-loudness.js
@@ -0,0 +1,12 @@
+var audioTestLoudness = {
+    type: jsPsychAudioButtonResponse,
+    stimulus: 'sound/bruitrose.wav',
+    choices: ['Continuer'],
+    response_allowed_while_playing: true,
+    prompt: ` <p>Réglez le volume de votre casque de façon confortable.<p>
+<p>Vous devez entendre clairement le son, et il ne doit être ni trop <strong>fort</strong> ni trop <strong>faible</strong>.</p>
+<p>Une fois le niveau réglé, veuillez ne plus le modifier jusqu'à la fin de l'expérience.</p>
+`
+};
+
+timeline.push(audioTestLoudness);
