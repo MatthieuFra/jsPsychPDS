828482b8b41e1e0da4f7848af09b19c4832f5754
MatthieuFra <fraticelli.matthieu@hotmail.fr>
2022-12-09 15:56:04 +0100

    translate to EN and add js file

---
+++ demos-tasks/sorting/js/sorting.js
@@ -0,0 +1,10 @@
+var single_feature_trial = {
+    type: jsPsychAudioTokens,
+    ratingtype: 'cluster',
+    stimuli: sounds,
+    force_listen: false,
+    loop: true,
+    prompt: '<p>Arrange these sounds in the way that seems most appropriate to you.</p>'
+}
+
+timeline.push(single_feature_trial);
